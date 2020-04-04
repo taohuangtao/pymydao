d58d38cb02ab812372dbdd49663b03624f391ac7
黄涛 <huangtao@cditv.tv>
2020-04-04 22:50:59 +0800

    修复多次关闭问题

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pymydao',
-      version='0.0.5',
+      version='0.0.6',
       description='mysql dao',
       url='https://github.com/taohuangtao/pymydao',
       author='huangtao',
