b49cc3f9683481a953911ccc9c57d72d40fb55ea
黄涛 <huangtao@cditv.tv>
2020-04-04 17:55:02 +0800

    更新版本

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pymydao',
-      version='0.0.4',
+      version='0.0.5',
       description='mysql dao',
       url='https://github.com/taohuangtao/pymydao',
       author='huangtao',
